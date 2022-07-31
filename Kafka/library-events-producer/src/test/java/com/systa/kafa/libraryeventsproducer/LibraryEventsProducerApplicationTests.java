b5ddf8d43e5ce14180c5274e2894c288ea762020
Mohsin0568 <mohsin68.murtuza@gmail.com>
2022-07-31 16:11:06 +0530

    initial commit for kafka library event project

---
+++ Kafka/library-events-producer/src/test/java/com/systa/kafa/libraryeventsproducer/LibraryEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systa.kafa.libraryeventsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
