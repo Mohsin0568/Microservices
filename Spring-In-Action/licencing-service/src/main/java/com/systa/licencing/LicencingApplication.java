a158eb0101f0e6acd0e699e10e47fda88338994d
Mohsin0568 <mohsin68.murtuza@gmail.com>
2022-12-03 17:16:53 +0530

    Refactored application

---
+++ Spring-In-Action/licencing-service/src/main/java/com/systa/licencing/LicencingApplication.java
@@ -0,0 +1,33 @@
+package com.systa.licencing;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@SpringBootApplication
+public class LicencingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicencingApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(false);
+		messageSource.setBasenames("messages");
+		return messageSource;
+	}
+
+}
---
+++ Spring-In-Action/licencing-service/src/test/java/com/systa/licencing/LicencingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systa.licencing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicencingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
