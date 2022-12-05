765141ff5859b6a4471c727ce24cb3c1458ad9e3
Mohsin0568 <mohsin68.murtuza@gmail.com>
2022-12-05 19:59:13 +0530

    added new config server for sping boot in action project

---
+++ Spring-In-Action/config-server/src/test/java/com/systa/action/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systa.action.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
